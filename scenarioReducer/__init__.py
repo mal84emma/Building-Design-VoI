6ff70124e6458c37141aee7efb546cce8fdf0c32
mal84emma <mal84@cam.ac.uk>
2024-05-01 12:38:36 +0100

    Implement test of scenario reduction using aggregate profile stats

---
+++ scenarioReducer/__init__.py
@@ -0,0 +1,15 @@
+from .fast_forward import Fast_forward
+from .scenario_reducer import Scenario_reducer
+
+__version__ = "0.1.0edited"
+__author__ = "Daniele Giovanni Gioia, pdb5627 - mal84 edited"
+# package edited to return indices of the reduced set for cross-refernecing
+
+
+__all__ = [
+    "Fast_forward",
+    "Scenario_reducer",
+    # Other
+    "__author__",
+    "__version__",
+]
