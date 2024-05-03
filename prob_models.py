68d1d3c874f55f3f7b9714186aba202bc4388861
Max Langtry <89249742+mal84emma@users.noreply.github.com>
2024-05-03 22:13:03 +0100

    Scaffold out voi calc files

---
+++ prob_models.py
@@ -0,0 +1,11 @@
+"""Definition of probabilistic models for decision problem."""
+
+def prior_model(building_ids,years):
+    """ToDo"""
+
+    return ...
+
+def posterior_model(sampled_id,building_ids,years):
+    """ToDo"""
+
+    return ...
