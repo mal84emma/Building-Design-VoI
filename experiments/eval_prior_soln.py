b8fc5b89b667a45e564a0eae81cdaa3b60a6f8b4
mal84 <mal84@cam.ac.uk>
2024-05-13 21:35:45 +0100

    Test prior eval MC convergence

--- experiments/eval_prior_soln.py
+++ experiments/eval_prior_soln.py
@@ -6,37 +6,43 @@ import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 # run using `python -m experiments.{fname}`
 
+import warnings
 from utils import data_handling
+import multiprocessing as mp
 from energy_system import evaulate_multi_system_scenarios
 
 if __name__ == '__main__':
 
-    from experiments.expt_config import *
+    with warnings.catch_warnings():
+        # filter pandas warnings, `DeprecationWarning: np.find_common_type is deprecated.`
+        warnings.simplefilter("ignore", category=DeprecationWarning)
 
-    # Load prior scenario samples.
-    scenarios_path = os.path.join('experiments','results','sampled_scenarios.csv')
-    scenarios = data_handling.load_scenarios(scenarios_path)
-    n_buildings = scenarios.shape[1]
+        from experiments.expt_config import *
 
-    # Load prior optimal system design.
-    design_results_path = os.path.join('experiments','results','prior_design_results.csv')
-    system_design = data_handling.load_design_results(design_results_path)
+        # Load prior scenario samples.
+        scenarios_path = os.path.join('experiments','results','sampled_scenarios.csv')
+        scenarios = data_handling.load_scenarios(scenarios_path)
+        n_buildings = scenarios.shape[1]
 
-    # Evaluate prior optimal system design.
-    n_processes = 6
+        # Load prior optimal system design.
+        design_results_path = os.path.join('experiments','results','prior_design_results.csv')
+        system_design = data_handling.load_design_results(design_results_path)
 
-    mean_cost, eval_results = evaulate_multi_system_scenarios(
-        scenarios[:60],
-        system_design,
-        dataset_dir,
-        building_fname_pattern,
-        design=True,
-        cost_dict=cost_dict,
-        n_processes=n_processes,
-        show_progress=True
-    )
-    print(mean_cost)
+        # Evaluate prior optimal system design.
+        n_processes = mp.cpu_count()
 
-    # Save results.
-    out_path = os.path.join('experiments','results','prior_eval_results.csv')
-    data_handling.save_eval_results(eval_results, system_design, scenarios[:60], out_path)
+        mean_cost, eval_results = evaulate_multi_system_scenarios(
+            scenarios,
+            system_design,
+            dataset_dir,
+            building_fname_pattern,
+            design=True,
+            cost_dict=cost_dict,
+            n_processes=n_processes,
+            show_progress=True
+        )
+        print(mean_cost)
+
+        # Save results.
+        out_path = os.path.join('experiments','results','prior_eval_results.csv')
+        data_handling.save_eval_results(eval_results, system_design, scenarios, out_path)
