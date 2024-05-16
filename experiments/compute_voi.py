4c2d3e4cc06b6e9de680379b81d819ecc68420b9
mal84emma <mal84@cam.ac.uk>
2024-05-16 17:15:13 +0100

    Add cost standard dev reporting to voi script

--- experiments/compute_voi.py
+++ experiments/compute_voi.py
@@ -17,7 +17,9 @@ if __name__ == '__main__':
     prior_eval_results_path = os.path.join('experiments','results','prior','prior_eval_results.csv')
     prior_eval_results = data_handling.load_eval_results(prior_eval_results_path)
     prior_mean_cost = np.mean([res['objective'] for res in prior_eval_results])
+    prior_cost_std = np.std([res['objective'] for res in prior_eval_results])
     print(f'Prior Mean Cost: {prior_mean_cost}')
+    print(f'Prior Cost Std: {prior_cost_std}')
 
     # Load posterior eval results.
     # ============================
@@ -27,7 +29,9 @@ if __name__ == '__main__':
         posterior_eval_results_files = [file for file in os.listdir(posterior_eval_results_dir) if file.endswith(".csv")]
         post_eval_results = [data_handling.load_eval_results(os.path.join(posterior_eval_results_dir, file)) for file in posterior_eval_results_files]
         posterior_mean_cost = np.mean([np.mean([res['objective'] for res in scenario_results]) for scenario_results in post_eval_results])
+        posterior_mean_cost_std = np.mean([np.std([res['objective'] for res in scenario_results]) for scenario_results in post_eval_results])
         print(f'Posterior Mean Cost ({info_type} info): {posterior_mean_cost}')
+        print(f'Posterior Cost Std ({info_type} info): {posterior_mean_cost_std}')
 
         # Compute Value of Information.
         # =============================
