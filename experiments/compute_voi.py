788c03e72b07a6c2cb36c52cf57aedf578792e47
mal84emma <mal84@cam.ac.uk>
2024-05-16 23:25:49 +0100

    Clean syntax and add min-max reporting

--- experiments/compute_voi.py
+++ experiments/compute_voi.py
@@ -16,10 +16,12 @@ if __name__ == '__main__':
     # ========================
     prior_eval_results_path = os.path.join('experiments','results','prior','prior_eval_results.csv')
     prior_eval_results = data_handling.load_eval_results(prior_eval_results_path)
-    prior_mean_cost = np.mean([res['objective'] for res in prior_eval_results])
-    prior_cost_std = np.std([res['objective'] for res in prior_eval_results])
+    prior_costs = [res['objective'] for res in prior_eval_results]
+    prior_mean_cost = np.mean(prior_costs)
+    prior_cost_std = np.std(prior_costs)
     print(f'Prior Mean Cost: {prior_mean_cost}')
     print(f'Prior Cost Std: {prior_cost_std}')
+    print('Overall min-max: ', np.min(prior_costs), np.max(prior_costs))
 
     # Load posterior eval results.
     # ============================
@@ -28,13 +30,16 @@ if __name__ == '__main__':
         posterior_eval_results_dir = os.path.join('experiments','results',f'posterior_{info_type}_info','evals')
         posterior_eval_results_files = [file for file in os.listdir(posterior_eval_results_dir) if file.endswith(".csv")]
         post_eval_results = [data_handling.load_eval_results(os.path.join(posterior_eval_results_dir, file)) for file in posterior_eval_results_files]
-        posterior_mean_cost = np.mean([np.mean([res['objective'] for res in scenario_results]) for scenario_results in post_eval_results])
+        post_mean_costs = [np.mean([res['objective'] for res in scenario_results]) for scenario_results in post_eval_results]
+        posterior_mean_cost = np.mean(post_mean_costs)
         posterior_mean_cost_std = np.mean([np.std([res['objective'] for res in scenario_results]) for scenario_results in post_eval_results])
         print(f'Posterior Mean Cost ({info_type} info): {posterior_mean_cost}')
         print(f'Posterior Cost Std ({info_type} info): {posterior_mean_cost_std}')
+        print('Mean min-max: ', np.min(post_mean_costs), np.max(post_mean_costs))
+        print('Overall min-max: ', *[f([res['objective'] for scenario_results in post_eval_results for res in scenario_results]) for f in [np.min, np.max]])
 
         # Compute Value of Information.
         # =============================
         voi = prior_mean_cost - posterior_mean_cost
         print(f'Value of Information ({info_type} info): {voi}')
-        print(f'VOI as % of Prior Mean Cost: {voi/prior_mean_cost*100:.2f}%')
+        print(f'VOI as % of Prior Mean Cost: {voi/prior_mean_cost*100:.3f}%')
