61fed01d1ace7ec3844aa24b85795d891f37a32f
mal84emma <mal84@cam.ac.uk>
2024-05-29 14:32:49 +0100

    Update energy_system test run

--- energy_system.py
+++ energy_system.py
@@ -428,16 +428,18 @@ if __name__ == '__main__':
 
         years = list(range(2012, 2018))
         ids = [0, 4, 8, 19, 25, 40, 58, 102, 104] # 118
-        n_buildings = 8
+        n_buildings = 3
 
         cost_dict = {
-            'carbon': 1.0, #5e-1, # $/kgCO2
-            'battery': 1e3, #1e3, # $/kWh
-            'solar': 1e3, #2e3, # $/kWp
+            'carbon': 1.0, # $/kgCO2
+            'battery': 750.0, # $/kWh
+            'solar': 1500.0, # $/kWp
             'grid_capacity': 25e-2/0.95, # $/kW/day - note, this is waaay more expensive that current
             'grid_excess': 100e-2/0.95, # $/kW/day - note, this is a waaay bigger penalty than current
             'opex_factor': 20,
-            'battery_power_ratio': 0.4
+            'battery_power_ratio': 0.4, # kW/kWh
+            'grid_con_safety_factor': 1.25, # safety factor for grid connection capacity
+            'cntrl_grid_cap_margin': 0.01 # margin for grid capacity in control scheme to prevent drift
         }
 
         np.random.seed(0)
@@ -446,7 +448,7 @@ if __name__ == '__main__':
 
         # test system design
         design_results = design_system(scenarios, dataset_dir, building_fname_pattern, cost_dict,
-                                        solver_kwargs=solver_kwargs, num_reduced_scenarios=10,
+                                        solver_kwargs=solver_kwargs, num_reduced_scenarios=5,
                                         show_progress=True
                                     )
 
