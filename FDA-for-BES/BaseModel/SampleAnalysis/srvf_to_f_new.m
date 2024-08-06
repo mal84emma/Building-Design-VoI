358ccec5e610519d6e4256991a2125ceab593feb
mal84emma <mal84@cam.ac.uk>
2024-08-06 10:55:57 +0100

    Remove FDA files

--- FDA-for-BES/BaseModel/SampleAnalysis/srvf_to_f_new.m
+++
@@ -1,9 +0,0 @@
-function f = srvf_to_f_new(q,time,fo)
-%
-% Code from https://github.com/jdtuck/fdasrvf_MATLAB
-%
-integrand = q.*abs(q);
-f = cumsum(integrand);
-for i = 1:length(fo)
-    f(:,i) = fo(i) + f(:,i);
-end
--- FDA-for-BES/RetrofitModel/srvf_to_f_new.m
+++
@@ -1,6 +0,0 @@
-function f = srvf_to_f_new(q,time,fo)
-integrand = q.*abs(q);
-f = cumsum(integrand);
-for i = 1:length(fo)
-    f(:,i) = fo(i) + f(:,i);
-end
